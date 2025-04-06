dba6bc706427dc4dfb612a41ea664271ffa843df
Erykson-labnight <ericgm791@gmail.com>
2025-04-06 07:50:13 -0600

    Password

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,20 @@
                 <li><a href="{{ route('genders.index') }}">Genders</a></li>
                 <li><a href="{{ route('universes.index') }}">Universes</a></li>
                 <li><a href="{{ route('superheroes.index') }}">Superheroes</a></li>
+
+                @auth
+                    <li><a href="{{ route('dashboard') }}">Dashboard</a></li>
+                    <li><a href="{{ route('profile.edit') }}">Profile</a></li>
+                    <li>
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <button type="submit">Logout</button>
+                        </form>
+                    </li>
+                @else
+                    <li><a href="{{ route('login') }}">Login</a></li>
+                    <li><a href="{{ route('register') }}">Register</a></li>
+                @endauth
             </ul>
         </nav>
     </header>
